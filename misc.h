5d46a83ed322df1f7596d30705b222ad2932484a
Alexander Dergachev <dergachev88@gmail.com>
2015-10-14 15:23:14 +0300

    Output functions implemented
    Output function templates implemented for binary and text versions

---
+++ misc.h
@@ -0,0 +1,38 @@
+#ifndef MISC_H_INCLUDED
+#define MISC_H_INCLUDED
+
+#include <complex>
+#include <fstream>
+
+template <typename T>
+inline T sqr(T x)
+{
+	return x*x;
+}
+
+template <typename T>
+int OutBinary(const char *file_name, const T *source, size_t length, size_t step)
+{
+    std::ofstream out(file_name);
+    if (!out)
+        return 1;
+    if (step == 0)
+        out.write(source, length * sizeof(*source));
+    else
+        for (int i = 0; i < length; i += step)
+            out.write(source + i, sizeof(*source));
+    return 0;
+}
+
+template <typename T>
+int OutText(const char * file_name, const T *source, size_t length, size_t step)
+{
+    std::ofstream out(file_name);
+    if (!out)
+        return 1;
+    for (int i = 0; i < length; i += step)
+        out << source[i] << '\n';
+    return 0;
+}
+
+#endif  // MISC_H_INCLUDED
